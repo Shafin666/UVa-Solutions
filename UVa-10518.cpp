b7198f598f14925efa5ff6eef9831f3bbca110e6
Shafin666 <shafin1206@gmail.com>
2018-12-10 18:11:35 +0600

    code

--- UVa-10518.cpp
+++ UVa-10518.cpp
@@ -25,8 +25,8 @@ void multiply(ll a[n][n], ll b[n][n]) {
 
 ll power(ll f[n][n], ll N) {
 	ll M[3][3] = {{1, 1, 1},
-				   {1, 0, 0},
-				   {0, 0, 1}};
+		      {1, 0, 0},
+		      {0, 0, 1}};
 
 	if(N == 1) return (f[0][0] + f[0][1] + f[0][2]) % m;
 	power(f, N/2);
@@ -38,8 +38,8 @@ ll power(ll f[n][n], ll N) {
 
 ll find(ll n) {
 	ll M[3][3] = {{1, 1, 1},
-				   {1, 0, 0},
-				   {0, 0, 1}};
+		      {1, 0, 0},
+		      {0, 0, 1}};
 	return power(M, n-1);
 }
 
@@ -50,7 +50,7 @@ int main()
 
 	ll n, caseno = 0;
 
-	while(scanf("%lld%lld", &n, &m) && (n || m)) {
+	while(scanf("%lld %lld", &n, &m) && (n || m)) {
 		if(n == 0 || n == 1) { printf("Case %lld: %lld %lld %lld\n", ++caseno, n, m, 1 % m); continue; }
 		printf("Case %lld: %lld %lld %lld\n", ++caseno, n, m, find(n));
 	}	
